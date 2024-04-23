ff9fcd94d7d8932c7e49d81c26032fcc62df971d
Amelia Caffrey <amelia.caffrey@zuhlke.com>
2024-04-23 18:12:20 +0100

    implement section 2

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+      const { monsters } = this.props;
+        return <div>Hello</div>;
+      }
+}
+
+export default CardList;
